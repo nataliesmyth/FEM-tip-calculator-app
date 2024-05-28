278e83eb4e03d47312f6bb4dcb2aedd981c18e4e
Nataliesmyth <nataliesmyth11@gmail.com>
2024-05-28 01:32:04 -0400

    input functionality

--- app.js
+++ app.js
@@ -1,48 +1,55 @@
+const inputFields = document.querySelector('.input');
+const end = inputFields.value.length;
+
+// move focus to end of input field
+inputFields.setSelectionRange(end, end);
+inputFields.focus();
+
+// user data
 const inputBillTotal = document.getElementById('billInput');
-const inputPeopleTotal = document.getElementById('inputNumberOfPeople');
+const inputPeopleTotal = document.getElementById('peopleInput');
+
 const buttons = document.querySelectorAll('.tip-percentage-btn');
 const customTip = document.getElementById('custom-tip');
-console.log(buttons);
+
 let currBillTotal = 0;
+let currPeopleTotal;
 let tipPercentage = 0;
 let tip;
 
+// Update user interface
 let tipResult = document.getElementById('tipResult');
 let billResult = document.getElementById('billResult');
-console.log(tipResult)
 
 let inputBillActive = false;
 let inputPeopleActive = false;
 let btnActive = false;
 let inputTipActive = false;
 
-console.log(inputBillTotal.value);
-console.log('inputPeopleTotal', inputPeopleTotal.value);
+console.log(typeof parseInt(inputPeopleTotal.value));
 
-// update bill
-// save inputBillTotal.value to variable currBillTotal
-// check if inputBillTotal.value === currBillTotal
-// if not equal
-// set currBillTotal to inputBillTotal.value
-// run update calculator fn
-const handleAddingZero = () => {
-    console.log('handle adding zero function says hi!!!');
+const handleBillResultLength = () => {
     let regex = /\.+/g;
     let resultAmt = tip.toString();
-    console.log('resultAmt', resultAmt);
-    console.log(resultAmt.match(regex));
 
     if (resultAmt.match(regex) !== null) {
-        resultAmt.split(' ');
+        // resultAmt.split(' ');
         resultAmt = Array.from(resultAmt);
-        console.log(resultAmt);
-        let sliceStart = resultAmt.indexOf('.')
-        console.log(sliceStart)
-        resultAmt = resultAmt.slice(sliceStart)
-        console.log(resultAmt)
-        if (resultAmt.length < 3) {
+        console.log('resultAmt', resultAmt);
+        let decimalPoint = resultAmt.indexOf('.');
+        let afterDecimal = resultAmt.slice(decimalPoint);
+        console.log('tip', tip)
+        console.log('afterDecimal', afterDecimal)
+        
+        if (afterDecimal.length < 3) {
             tip = tip + '0';
-            console.log(tip);
+            tipResult.innerText = '$' + tip;
+        } 
+        
+        if (resultAmt.length > 5) {
+            const beforeDecimal = resultAmt.slice(0, decimalPoint).join('');
+            afterDecimal = afterDecimal.slice(0, 5).join('');
+            tip = beforeDecimal + afterDecimal;
             tipResult.innerText = '$' + tip;
         }
     }
@@ -50,49 +57,74 @@ const handleAddingZero = () => {
 
 const checkCalculator = () => {
     tip = (parseFloat(currBillTotal) * parseFloat(tipPercentage) / 100);
-    console.log(tip)
+
     if (inputPeopleTotal.value > 1) {
         tip = tip / parseFloat(inputPeopleTotal.value);
     }
     tipResult.innerText = '$' + tip;
 
-    handleAddingZero();
+    handleBillResultLength();
 }
 
 checkCalculator();
 
-// TIP CALCULATOR
-
-// 
-
-function handleTipBtnClick() {
+function handleTipInput() {
     inputTipActive = true;
-    console.log('tip btn clicked!');
     checkCalculator();
 }
-
-// buttons.forEach((button) => {
-    //     button.addEventListener('click', handleTipBtnClick);
-    // });
     
-    buttons.forEach((button) => {
-        button.addEventListener('click', () => {
-            console.log(button.dataset.percent);
-            tipPercentage = button.dataset.percent;
-            console.log('typeof tipPercentage: ',typeof tipPercentage)
-            handleTipBtnClick();
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        tipPercentage = button.dataset.percent;
+        handleTipInput();
     });
 });
 
-inputBillTotal.addEventListener('input', () => {
+
+const handleBillInput = () => {
     currBillTotal = inputBillTotal.value;
+    console.log('currBillTotal: ', currBillTotal)
     billResult.innerText = '$' + currBillTotal;
-    console.log(currBillTotal);
     if (inputBillTotal !== '') {
         inputBillActive = true;
+        inputPeopleActive = true;
     } else {
         checkCalculator();
     }
-})
+}
 
-// customTip.addEventListener('input', handleTipBtnClick);
+const handlePeopleInput = () => {
+
+    console.log(inputPeopleTotal.value);
+    currPeopleTotal = inputPeopleTotal.value;
+    console.log('currPeopleTotal: ', currPeopleTotal);
+
+    if (inputPeopleTotal !== '') {
+
+        inputPeopleTotal.innerText = '$' + currPeopleTotal;
+        inputPeopleActive = true;
+
+        if (parseInt(currPeopleTotal) === 0) {
+            inputPeopleTotal.style.borderColor = 'red';
+            inputPeopleTotal.style.outline = 'none';
+            inputPeopleTotal.style.borderRadius = '5px';
+        } else if (parseInt(currPeopleTotal) > 0) {
+            inputPeopleTotal.style.borderColor = 'green';
+            inputPeopleTotal.style.outline = 'none';
+            inputPeopleTotal.style.borderRadius = '5px';
+        }
+    } else {
+        checkCalculator();
+    }
+}
+
+
+handlePeopleInput();
+
+
+window.addEventListener('load', handleBillInput);
+inputBillTotal.addEventListener('input', handleBillInput);
+inputPeopleTotal.addEventListener('input', handlePeopleInput);
+
+
+// customTip.addEventListener('input', handleTipInput);
